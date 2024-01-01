bcad49c9f8c74a59f9be03585e5c47990260544f
alirezarezaei1 <rly01309@gmail.com>
2024-01-01 14:45:26 +0330

    output

--- second e.cpp
+++ second e.cpp
@@ -2,9 +2,13 @@
 #include <vector>
 #include <array>
 using namespace std;
-string ekh(string x);
+
 int main(){
-    int i = 0;
-    
+    int i = 0,x=0;
+    int ar[6]={1,1,2,2,2,8};
+    while(i!=6){
+        cout<<ar[i]-x<<" ";
+        i++;
+    }    
     return 0;
 }
